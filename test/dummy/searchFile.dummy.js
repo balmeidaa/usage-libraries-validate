e8dcc8b206f83d56bcf209816f6094a1797dbff0
sandra.gil <54ndr4g@gmail.com>
2018-04-20 21:17:05 -0500

    searchFile. test proxyquire

--- test/dummy/searchFile.dummy.js
+++ test/dummy/searchFile.dummy.js
@@ -2,17 +2,18 @@
 
 const sinon = require('sinon');
 const fileReader = require('fs');
+const proxyquire = require ('proxyquire'); 
 
-let fileStub = sinon.stub(fileReader, 'readFileSync').callsFake(()=>{
-return false;
-});
+const fileStub = sinon.stub()
+.withArgs('dummy.js')
+.returns('anything');
+ const searchFile = proxyquire('../../lib/searchFile', {fs: {readFileSync: fileStub}} );
 
 const fileList = ['dummy.js'];
 const libraryList = ['jest', 'sinon', 'fs'];
 const libraryFound = new Map().set('jest', false)
 	.set('sinon', false)
 	.set('fs', false);
-
 
 
 const expectingMapLibs = new Map().set('jest', false)
