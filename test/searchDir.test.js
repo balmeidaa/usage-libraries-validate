bfed8c23e792782c4625acd9bd4007fd7c12adf2
sandra.gil <54ndr4g@gmail.com>
2018-04-20 19:52:17 -0500

    fix(test Dir)

--- test/searchDir.test.js
+++ test/searchDir.test.js
@@ -3,8 +3,21 @@
 const core = require('../lib/searchDir');
 const dummy = require('./dummy/searchDir.dummy');
 
+const fileSystem = require('fs');
+const sinon= require('sinon');
+/*
+* path sea correcto o valido
+* directorio valido 
+*/
+test('validateNameDir',() =>{
+expect(searchDir.validateNameDir(constsearchDirDummy.pathDummy, constsearchDirDummy.fileDummy))
+.toBe(true);
+}
+);
+/*
 test('_getAllFilesFromFolder without folders', () => {
     expect(core._getAllFilesFromFolder('./'))
         .toEqual(expect.arrayContaining([]));
     dummy.dirStub.restore();
 });
+*/
